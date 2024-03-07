8770088772723fb1e4bbd86e93773083853c4611
Kieran Hanrahan <kieran@knhn.me>
2024-03-07 12:20:43 -0500

    Update FlameListWidget class
    Update to most recent version of FlameListWidget to better match the UI
    in Flame 2022 and forward.

--- find_folder.py
+++ find_folder.py
@@ -200,33 +200,86 @@ class FlameLineEdit(QtWidgets.QLineEdit):
 
 
 class FlameListWidget(QtWidgets.QListWidget):
-    """
+    '''
     Custom Qt Flame List Widget
-    To use:
-    list_widget = FlameListWidget(window)
-    """
 
-    def __init__(self, parent_window, *args, **kwargs):
-        super(FlameListWidget, self).__init__(*args, **kwargs)
+    FlameListWidget([min_width=200, max_width=2000, min_height=250, max_height=2000])
 
-        self.setMinimumSize(500, 250)
-        self.setParent(parent_window)
-        self.setFocusPolicy(QtCore.Qt.NoFocus)
-        # only want 1 selection possible.  no multi selection.
-        #self.setSelectionMode(QtWidgets.QAbstractItemView.ExtendedSelection)
-        self.setSpacing(3)
-        self.setAlternatingRowColors(True)
+    Example:
+
+        list_widget = FlameListWidget()
+    '''
+
+    def __init__(
+            self, min_width: Optional[int]=200, max_width: Optional[int]=2000,
+            min_height: Optional[int]=250, max_height: Optional[int]=2000):
+        super(FlameListWidget, self).__init__()
+
+        # Check argument types
+
+        if not isinstance(min_width, int):
+            raise TypeError('FlameListWidget: min_width must be integer.')
+        if not isinstance(max_width, int):
+            raise TypeError('FlameListWidget: max_width must be integer.')
+        if not isinstance(min_height, int):
+            raise TypeError('FlameListWidget: min_height must be integer.')
+        if not isinstance(max_height, int):
+            raise TypeError('FlameListWidget: max_height must be integer.')
+
+        # Build list widget
+
+        self.setMinimumWidth(min_width)
+        self.setMaximumWidth(max_width)
+        self.setMinimumHeight(min_height)
+        self.setMaximumHeight(max_height)
+        self.spacing()
         self.setUniformItemSizes(True)
-        self.setStyleSheet("""
+        self.setFocusPolicy(QtCore.Qt.NoFocus)
+        self.setSelectionMode(QtWidgets.QAbstractItemView.ExtendedSelection)
+        self.setAlternatingRowColors(True)
+        self.setStyleSheet('''
             QListWidget {
-                color: #9a9a9a;
-                background-color: #2a2a2a;
-                alternate-background-color: #2d2d2d;
-                outline: none;
+                color: rgb(154, 154, 154);
+                background-color: rgb(30, 30, 30);
+                alternate-background-color: rgb(36, 36, 36);
+                outline: 3px rgb(0, 0, 0);
                 font: 14px "Discreet"}
             QListWidget::item:selected {
-                color: #d9d9d9;
-                background-color: #474747}""")
+                color: rgb(217, 217, 217);
+                background-color: rgb(102, 102, 102);
+                border: 1px solid rgb(102, 102, 102)}
+            QScrollBar {
+                background: rgb(61, 61, 61)}
+            QScrollBar::handle {
+                background: rgb(31, 31, 31)}
+            QScrollBar::add-line:vertical {
+                border: none;
+                background: none;
+                width: 0px;
+                height: 0px}
+            QScrollBar::sub-line:vertical {
+                border: none;
+                background: none;
+                width: 0px;
+                height: 0px}
+            QScrollBar {
+                background: rgb(61, 61, 61)}
+            QScrollBar::handle {
+                background: rgb(31, 31, 31)}
+            QScrollBar::add-line:horizontal {
+                border: none;
+                background: none;
+                width: 0px;
+                height: 0px}
+            QScrollBar::sub-line:horizontal {
+                border: none;
+                background: none;
+                width: 0px;
+                height: 0px}
+            QToolTip {
+                color: rgb(170, 170, 170);
+                background-color: rgb(71, 71, 71);
+                border: 10px solid rgb(71, 71, 71)}''')
 
 
 class FlameMessageBox(QtWidgets.QMessageBox):
